eff9161fb5ba360bac21080b28a35f55f8d031d6
YaroslavLazorenko <lazorenko85@gmail.com>
2023-08-11 04:07:31 +0300

    Add typescript in project

---
+++ src/components/ContactItem/ContactItem.tsx
@@ -0,0 +1,40 @@
+// import { PropTypes } from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+import { phonebookOperations, phonebookSelectors } from 'redux/phonebook';
+import styles from './ContactItem.module.css';
+
+type Props = {
+    
+}
+
+const ContactItem: React.FC<Props> = ({ contact }) => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(phonebookSelectors.getLoadingStatus);
+
+  return (
+    <li className={styles.contactItem}>
+      <span className={styles.contactText}>
+        {contact.name}: {contact.number}
+      </span>
+      <Button
+        onClick={() => dispatch(phonebookOperations.deleteContact(contact.id))}
+        disabled={isLoading}
+        variant="contained"
+        size="small"
+      >
+        Delete
+      </Button>
+    </li>
+  );
+};
+
+// ContactItem.propTypes = {
+//   contact: PropTypes.shape({
+//     id: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     number: PropTypes.string.isRequired,
+//   }),
+// };
+
+export default ContactItem;
