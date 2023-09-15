6436b54f49f17bbfa86e88f71fcebf906cd02116
YaroslavLazorenko <lazorenko85@gmail.com>
2023-09-15 20:42:27 +0300

    Add typescript to AuthNav

---
+++ src/components/AuthNav/AuthNav.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import { ROUTES } from 'consts';
+import styles from './AuthNav.module.css';
+
+const AuthNav: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <NavLink
+        to={ROUTES.REGISTER}
+        className={({ isActive }) => 'nav-link' + (isActive ? ' activated' : '')}
+      >
+        Sign Up
+      </NavLink>
+      <NavLink
+        to={ROUTES.LOGIN}
+        className={({ isActive }) => 'nav-link' + (isActive ? ' activated' : '')}
+      >
+        Login
+      </NavLink>
+    </div>
+  );
+};
+
+export default AuthNav;
