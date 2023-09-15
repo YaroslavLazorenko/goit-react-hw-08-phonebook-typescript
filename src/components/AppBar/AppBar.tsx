11ffa890d8193e9bc43cf61e7eb0e9b0fee5eac8
YaroslavLazorenko <lazorenko85@gmail.com>
2023-09-16 00:34:00 +0300

    Add typescript to AppBar component

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,13 @@
+import { authSelectors } from 'redux/auth';
+import AuthNav from '../AuthNav';
+import UserMenu from '../UserMenu';
+import styles from './AppBar.module.css';
+import { useAppSelector } from 'hooks';
+
+const AppBar: React.FC  = () => {
+  const isLoggedIn = useAppSelector(authSelectors.getIsLoggedIn);
+
+  return <header className={styles.header}>{isLoggedIn ? <UserMenu /> : <AuthNav />}</header>;
+};
+
+export default AppBar;
